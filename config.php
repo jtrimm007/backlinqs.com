cbe0efc0c92a2d828989c160c9ef1c6ee7c06a1f
Joshua Trimm <jbthype@gmail.com>
2019-06-11 01:51:44 -0400

    Removed Personal Info
    Removed my database connection infromation

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /**
  * Description: Config file
  */
-define("USER", "backlinq_jtrimm");
-define("DATA", "backlinq_web");
-define("PASS", "B+A>rhdxEpGwI");
+define("USER", "YOUR DATABASE USER NAME");
+define("DATA", "YOUR DATABASE NAME");
+define("PASS", "YOUR DATABASE PASS");
 define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 define("HOST", $_SERVER['HTTP_HOST']);
 define("SERVER", $_SERVER['SERVER_NAME']);
